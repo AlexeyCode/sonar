dce64bc078b929c0f86b7d283752adab82adf8a9
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-09-30 22:39:53 +0300

    duplication checks

--- src/test/java/com/acme/basic/HelloWorldTest.java
+++ src/test/java/com/acme/basic/HelloWorldTest.java
@@ -8,4 +8,68 @@ public class HelloWorldTest {
   public void sayHello() {
     new HelloWorld().sayHello();
   }
+
+  @Test
+  public void sayHello1() {
+    new HelloWorld().sayHello();
+  }
+
+    @Test
+    public void sayHello2() {
+      new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello3() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello4() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello5() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello6() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello7() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello8() {
+    new HelloWorld().sayHello();
+  }
+
+  @Test
+  public void sayHello9() {
+    new HelloWorld().sayHello();
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+    System.out.println("HELLO");
+  }
+
+  @Test
+  public void sayHello10() {
+    new HelloWorld().sayHello();
+  }
 }
